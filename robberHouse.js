1ea06879eed79965c9521bed68a455431391853c
Joan Manuel Vasquez <yoloprogramo22@gmail.com>
2020-11-13 10:59:05 -0400

    Create robberHouse.js

---
+++ robberHouse.js
@@ -0,0 +1,11 @@
+let rob = function(nums) {
+    let previous = 0;
+    let current = 0;
+    let temp; 
+    for (let x of nums) {
+        temp = current; 
+        current = Math.max(x+previous, current);
+        previous = temp;
+    }
+    return current;
+}
